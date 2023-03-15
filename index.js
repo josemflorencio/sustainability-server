fb90b86c9de458a86cfcd61f66e9087a4d931ca1
Jose Florencio <josemflorencio09@gmail.com>
2023-03-15 15:11:10 -0400

    added dotenv package to project

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
-
+const dotenv = require('dotenv')
 const Register = require('./routes/register')
 const Reviews = require('./routes/reviews')
 const Locations = require('./routes/locations')
 
-const PORT = process.env.PORT || 4000
+dotenv.config()
+
+const PORT = process.env.PORT
 
 app.use('/register', Register)
 app.use('/reviews', Reviews)
 app.use('/locations', Locations)
 
-mongoose.connect("mongodb+srv://admin:3rdrmMd6zEEZKW0u@cluster0.wyntuck.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect(process.env.db)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
